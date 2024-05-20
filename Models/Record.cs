9723e1d38b62d442ba640991013b7424d58b0c33
Emin <dgbemin@gmail.com>
2024-05-20 04:58:33 +0300

    Erdem kaçar

--- Models/Record.cs
+++ Models/Record.cs
@@ -2,5 +2,9 @@
 {
     public class Record
     {
+        public DateTime Date { get; set; }
+        public string Title { get; set; }
+        public string Path { get; set; }
+        public int PatientId { get; set; }
     }
 }
