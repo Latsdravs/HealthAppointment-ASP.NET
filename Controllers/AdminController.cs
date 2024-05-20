96dbf9f60bcbac61f6d510f7da17ae592d74d083
Emin <dgbemin@gmail.com>
2024-05-20 06:36:08 +0300

    Revert "aneyy"
    This reverts commit 8897af54cee3bcddcfa6e7a7d22d6964dd35b9ac.

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -110,7 +110,6 @@ namespace TheApp.Controllers
                 int Id = wrap.Data;
                 response.StatusCode = wrap.StatusCode;
                 response.StatusMessage = wrap.StatusMessage;
-                if (wrap.Data == 0) response.StatusMessage += "aneyyy";
                 if (wrap.StatusCode == 100) return response;
 
 
